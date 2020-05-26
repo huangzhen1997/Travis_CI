7ed325d020e3e24b4eeb2eab8b7ed2b6fcd53bca
Zhen Huang <zhuang38@hawk.iit.edu>
2020-05-26 11:37:55 -0500

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+test('starter test', () => {
+  const value = 1;
+  expect(value).toEqual(1);
 });
